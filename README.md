45f5ceacaca9397363022e78514b945df1d17982
Daniel Foote <freefoote@dview.net>
2013-03-28 09:52:03 +0800

    Updated Sinatra app to return an image and different HTML. Updated the manifest, and added a README file to match.

---
+++ README.md
@@ -0,0 +1,16 @@
+Sample Ruby Simple
+==================
+
+This application is the simplest possible Ruby Sinatra application. It
+is intended as a smoke test of the Ruby support in Paasmaker.
+
+How to deploy?
+--------------
+
+Just use the Git SCM, and enter the URL of the repository to deploy this
+application.
+
+What is Paasmaker?
+------------------
+
+Visit http://paasmaker.org for more information about Paasmaker.
--- app.rb
+++ app.rb
@@ -1,11 +1,31 @@
 require 'sinatra'
 
 get '/' do
-	"Hello, world!"
+	response = '<!DOCTYPE html>'
+	response += '<html>'
+	response += '  <head><title>Hello, world!</title></head>'
+	response += '  <body>'
+	response += '    <div style="text-align:center;margin-top:60px">'
+	response += '    <p><img src="p-white.png" alt=""></p>'
+	response += '    <h1>Hello, world!</h1>'
+	response += '    <p><a href="/environ">Environment</a></p>'
+	response += '    </div>'
+	response += '  </body>'
+	response += '</html>'
+
+	return response
+end
+
+get '/p-white.png' do
+	send_file 'p-white.png'
 end
 
 get '/environ' do
-	response = "<pre>"
+	response = "<!DOCTYPE html>"
+	response += "<html>"
+	response += "  <head><title>Environment</title></head>"
+	response += "  <body>"
+	response += "<pre>"
 	ENV.each_pair do |k,v|
 		response += k + " => " + v + "\n"
 	end
